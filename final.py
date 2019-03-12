6edcacb75a2b558c8aaa7a1af935e995bd95b3f7
Matt <matthew.johnson@ucsf.edu>
2019-03-12 16:47:37 -0700

    added new NN class

--- final.py
+++ final.py
@@ -37,11 +37,41 @@ class NeuralNet(object):
 	def learn(self, inputs):
 		return self.__sigmoid(np.dot(inputs, self.synaptic_weights))
 
+########################################################################################
+########################################################################################
+def sigmoid(x):
+    return 1.0/(1+ np.exp(-x))
+def sigmoid_derivative(x):
+    return x * (1.0 - x)
+class NeuralNetwork:
+    def __init__(self, x, y):
+        self.input      = x
+        self.weights1   = np.random.rand(self.input.shape[1],10)
+        self.weights2   = np.random.rand(10,1)
+        self.y          = y
+        self.output     = np.zeros(self.y.shape)
+    def feedforward(self):
+        self.layer1 = sigmoid(np.dot(self.input, self.weights1))
+        self.output = sigmoid(np.dot(self.layer1, self.weights2))
+    def backprop(self):
+        # application of the chain rule to find derivative of the loss function with respect to weights2 and weights1
+        d_weights2 = np.dot(self.layer1.T, (2*(self.y - self.output) * sigmoid_derivative(self.output)))
+        d_weights1 = np.dot(self.input.T,  (np.dot(2*(self.y - self.output) * sigmoid_derivative(self.output), self.weights2.T) * sigmoid_derivative(self.layer1)))
+
+        # update the weights with the derivative (slope) of the loss function
+        self.weights1 += d_weights1 #multply by learning rate
+        self.weights2 += d_weights2 #multply by learning rate
+
+inputs = np.array([[0, 1, 0], [1, 0, 1]]) #row is the observations columns are the features
+outputs = np.array([[1, 0]]).T
+nn = NeuralNetwork(inputs, outputs)
+for i in range(150):
+    nn.feedforward()
+    nn.backprop()
+    print(nn.output)
+
+
 if __name__ == "__main__":
-
-	#Initialize
-	neural_network = NeuralNet()
-
 	# The training set.
 	positives = []
 	with open('/Users/matt/OneDrive/UCSF/algorithms/final/rap1-lieb-positives.txt') as csv_file:
@@ -68,10 +98,13 @@ if __name__ == "__main__":
 	outputs_test = np.ones((137,1))
 	positives_encoded_array = np.asarray(seq_list)
 
-	inputs = np.array([[0, 1], [1, 0], [1, 0]])
+	inputs = np.array([[0, 1, 0, 1], [1, 0, 1, 1]]) #row is the observations columns are the features
 	outputs = np.array([[1, 0]]).T
 
 	# Train the neural network
+
+		#Initialize
+	neural_network = NeuralNet()
 	neural_network.train(inputs, outputs, 10000)
 
 	# Test the neural network with a test example.
